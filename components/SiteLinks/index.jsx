3749c716fc46faa12866abebe89173efbddc0396
Go7hic <gtfx0209@qq.com>
2016-08-17 01:12:59 +0800

    update css

--- components/SiteLinks/index.jsx
+++ components/SiteLinks/index.jsx
@@ -6,37 +6,40 @@ import './style.css'
 import '../../static/fonts/fontawesome/style.css'
 
 class SiteLinks extends React.Component {
-    render() {
+  render () {
+    return (
+      <div className="blog-social">
+        <ul>
+          <li>
+            <a href={config.siteTwitterUrl} target="_blank" rel="noopener noreferrer" title="Twitter"><i className="fa fa-twitter" /></a>
+          </li>
+          <li>
+            <a href={config.siteGithubUrl} target="_blank" rel="noopener noreferrer" title="Github"><i className="fa fa-github-alt" /></a>
+          </li>
+          <li>
+            <a href={config.siteEmailUrl} title="邮件"><i className="fa fa-envelope-o" /></a>
+          </li>
+          {/* <li>
+            <a href={ config.siteVkUrl }><i className='fa fa-vk'></i></a>
+          </li>
+          */}
+        </ul>
+        {/*
+        <ul>
+          <li>
+            <a href={ config.siteTelegramUrl }><i className='fa fa-paper-plane'></i></a>
+          </li>
 
-        return (
-            <div className='blog-social'>
-              <ul>
-                <li>
-                  <a href={ config.siteTwitterUrl }><i className='fa fa-twitter'></i></a>
-                </li>
-                <li>
-                  <a href={ config.siteGithubUrl }><i className='fa fa-github-alt'></i></a>
-                </li>
-                <li>
-                  <a href={ config.siteVkUrl }><i className='fa fa-vk'></i></a>
-                </li>
-              </ul>
-              <ul>
-                <li>
-                  <a href={ config.siteEmailUrl }><i className='fa fa-envelope-o'></i></a>
-                </li>
-                <li>
-                  <a href={ config.siteTelegramUrl }><i className='fa fa-paper-plane'></i></a>
-                </li>
-              </ul>
-              <ul>
-                <li>
-                  <a href={ config.siteRssUrl }><i className='fa fa-rss'></i></a>
-                </li>
-              </ul>
-            </div>
-            );
-    }
+        </ul>
+        <ul>
+          <li>
+            <a href={ config.siteRssUrl }><i className='fa fa-rss'></i></a>
+          </li>
+        </ul>
+        */}
+      </div>
+    )
+  }
 }
 
 export default SiteLinks
--- pages/pages/about/index.md
+++ pages/pages/about/index.md
@@ -4,6 +4,10 @@ layout: page
 path: "/about"
 ---
 
->每一个博客都有过去：http://dyygtfx.github.io
+#### About Blog
+
+每一个博客都有过去：http://dyygtfx.github.io
+
+#### About Author
 
 程序员
