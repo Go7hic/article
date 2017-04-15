089b347b6aae45d21b886ba054e24e166a551fb3
Go7hic <gtfx0209@qq.com>
2017-04-15 23:38:07 +0800

    update: blog 样式更新

--- html.js
+++ html.js
@@ -33,8 +33,8 @@ module.exports = React.createClass({
         <body>
           <div id="react-mount" dangerouslySetInnerHTML={{ __html: this.props.body }} />
           <footer>
-              <p className='copyright'>
-                &copy; 2016 All rights reserved.
+              <p className='copyright' style={{ textAlign: 'center' }}>
+                &copy; 2016-2017 All rights reserved
               </p>
           </footer>
           <script src={prefixLink(`/bundle.js?t=${BUILD_TIME}`)} />
